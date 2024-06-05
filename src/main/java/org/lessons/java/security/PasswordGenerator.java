8b2e0f4d2f9a278682b16a59acf3212fc9269cd4
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-06-05 20:30:51 +0200

    add : esercizio completato

--- src/main/java/org/lessons/java/security/PasswordGenerator.java
+++ src/main/java/org/lessons/java/security/PasswordGenerator.java
@@ -4,7 +4,37 @@ public class PasswordGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("ciao");
+		
+     //valori 
+	String name;
+	String username;
+	String favoriteColor;
+	byte day;
+	byte month;
+	short year;
+	
+	//definizione valori
+	name = "Nicolò-";
+	username = "De Giorgi-";
+    favoriteColor = "Arancione-";
+    day = 26;
+    month = 06;
+    year = 1999;
+    
+    //somma data di nascia
+    int somma = 26+6+1999;
+    
+    System.out.printf("Password:  "+ name + username + favoriteColor + somma );
+    
+    
+    
+    
+	
+			
+	
+	 
+	
+		
 	}
 
 }
